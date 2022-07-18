008988efbc38dcc003109694ddd642aa538077a5
Sven Czarnian <devel@svcz.de>
2022-07-18 21:41:18 +0200

    introduce the logger

---
+++ logging/Logger.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "sqlite3.h"
+
+namespace vacdm {
+namespace logging {
+    class Logger {
+    public:
+        enum class Level {
+            Debug,
+            Info,
+            Warning,
+            Error,
+            Critical,
+            System
+        };
+
+    private:
+        sqlite3* m_database;
+        Level    m_minimumLevel;
+
+        Logger();
+    public:
+        void setMinimumLevel(Level level);
+        void log(const std::string& component, Level level, const std::string& message);
+        static Logger& instance();
+    };
+}
+}
