4f99d1300b087ebfe598e20ada63aec56a73c9f0
LeoKle <leon.kleinschmidt@outlook.de>
2024-03-23 23:27:20 +0100

    init DataManager

---
+++ src/core/DataManager.cpp
@@ -0,0 +1,29 @@
+#include "DataManager.h"
+
+using namespace vacdm::core;
+using namespace std::chrono_literals;
+
+DataManager::DataManager() : m_pause(false), m_stop(false) { this->m_worker = std::thread(&DataManager::run, this); }
+
+DataManager::~DataManager() {
+    this->m_stop = true;
+    this->m_worker.join();
+}
+
+DataManager& DataManager::instance() {
+    static DataManager __instance;
+    return __instance;
+}
+
+void DataManager::run() {
+    std::size_t counter = 1;
+    while (true) {
+        std::this_thread::sleep_for(1s);
+        if (true == this->m_stop) return;
+        if (true == this->m_pause) continue;
+
+        // run every 5 seconds
+        if (counter++ % 5 != 0) return;
+    }
+}
+
---
+++ src/core/DataManager.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <list>
+#include <map>
+#include <mutex>
+#include <string>
+#include <thread>
+
+#include "types/Pilot.h"
+
+using namespace vacdm;
+
+namespace vacdm::core {
+class DataManager {
+   private:
+    DataManager();
+
+    std::thread m_worker;
+    bool m_pause;
+    bool m_stop;
+
+    void run();
+
+   public:
+    ~DataManager();
+    DataManager(const DataManager &) = delete;
+    DataManager(DataManager &&) = delete;
+
+    DataManager &operator=(const DataManager &) = delete;
+    DataManager &operator=(DataManager &&) = delete;
+    static DataManager &instance();
+
+   private:
+    std::mutex m_pilotLock;
+    std::map<std::string, std::array<types::Pilot, 3>> m_pilots;
+    std::mutex m_airportLock;
+    std::list<std::string> m_activeAirports;
+};
+}  // namespace vacdm::core
