756c79aebb0072dd277e4acc0527031213aaa20d
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-07-24 22:52:36 +0200

    Backend: add JsonBuilder

--- src/backend/Backend.cpp
+++ src/backend/Backend.cpp
@@ -3,7 +3,11 @@
 using namespace vacdm;
 using namespace vacdm::backend;
 
+#include <json/json.h>
+
 #include <iostream>
+
+#include "backend/JsonBuilder.h"
 
 BackendWebsocket::BackendWebsocket(const std::string& serverUrl) : m_serverUrl(serverUrl) {
     ix::initNetSystem();
@@ -36,32 +40,73 @@ BackendWebsocket::~BackendWebsocket() {
     ix::uninitNetSystem();
 }
 
-bool BackendWebsocket::patchPilot(const std::string& endpointUrl, const Json::Value& body) { return false; }
+bool BackendWebsocket::patchPilot(const std::string& endpointUrl, const Json::Value& body) {
+    (void)endpointUrl;  // using websockets we don't need the endpoint url
 
-bool BackendWebsocket::postInitialPilotData(const types::Pilot& data) { return false; }
-
-bool BackendWebsocket::sendTargetDpiNow(const types::Pilot& data) { return false; }
-
-bool BackendWebsocket::sendTargetDpiTarget(const types::Pilot& data) { return false; }
-
-bool BackendWebsocket::sendTargetDpiSequenced(const std::string& callsign,
-                                              const std::chrono::utc_clock::time_point& asat) {
+    m_webSocket.send(body.asString());
     return false;
 }
 
-bool BackendWebsocket::sendAtcDpi(const std::string& callsign, const std::chrono::utc_clock::time_point& aobt) {
-    return false;
+bool BackendWebsocket::postInitialPilotData(const types::Pilot& data) {
+    const auto message = JsonBuilder::buildInitialPilotData(data);
+
+    m_webSocket.send(message.asString());
+
+    return true;
 }
 
-bool BackendWebsocket::sendCustomDpiTaxioutTime(const std::string& callsign,
-                                                const std::chrono::utc_clock::time_point& exot) {
-    return false;
+bool BackendWebsocket::sendTargetDpiNow(const types::Pilot& data) {
+    const auto json = JsonBuilder::buildTargetDpiNow(data);
+
+    m_webSocket.send(json.asString());
+
+    return true;
 }
 
-bool BackendWebsocket::sendCustomDpiRequest(const std::string& callsign,
-                                            const std::chrono::utc_clock::time_point& timePoint,
-                                            const bool isAsrtUpdate) {
-    return false;
+bool BackendWebsocket::sendTargetDpiTarget(const types::Pilot& data) {
+    const auto json = JsonBuilder::buildTargetDpiTarget(data);
+
+    m_webSocket.send(json.asString());
+
+    return true;
 }
 
-bool BackendWebsocket::sendPilotDisconnect(const std::string& callsign) { return false; }
+bool BackendWebsocket::sendTargetDpiSequenced(const types::Pilot& data) {
+    const auto json = JsonBuilder::buildTargetDpiSequenced(data);
+
+    m_webSocket.send(json.asString());
+
+    return true;
+}
+
+bool BackendWebsocket::sendAtcDpi(const types::Pilot& data) {
+    const auto json = JsonBuilder::buildAtcDpi(data);
+
+    m_webSocket.send(json.asString());
+
+    return true;
+}
+
+bool BackendWebsocket::sendCustomDpiTaxioutTime(const types::Pilot& data) {
+    const auto json = JsonBuilder::buildCustomDpiTaxioutTime(data);
+
+    m_webSocket.send(json.asString());
+
+    return true;
+}
+
+bool BackendWebsocket::sendCustomDpiRequest(const types::Pilot& data, const bool isAsrtUpdate) {
+    const auto json = JsonBuilder::buildCustomDpiRequest(data, isAsrtUpdate);
+
+    m_webSocket.send(json.asString());
+
+    return true;
+}
+
+bool BackendWebsocket::sendPilotDisconnect(const std::string& callsign) {
+    const auto json = JsonBuilder::buildPilotDisconnect(callsign);
+
+    m_webSocket.send(json.asString());
+
+    return false;
+}
